bf0d08d2a6d62e01eed965fea644c32fdb76e9a4
Pietro Lepori <152228616+pietro-lepori@users.noreply.github.com>
2025-06-04 23:59:08 +0200

    Add ocaml exercises 10-18/99 from ocaml.org

--- ocaml/99problems.ml
+++ ocaml/99problems.ml
@@ -129,3 +129,102 @@ let encode =
   function
   | x::xs -> aux [] x 1 xs |> List.rev
   | [] -> []
+
+(* 11: modified run-length encoding *)
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+let encode2 l =
+  let rec scan acc last counter =
+    function
+    | x::xs when x = last -> (scan [@tailcal]) acc last (counter + 1) xs
+    | l' ->
+      let record = Many (counter, last) in
+      (restart [@tailcal]) (record::acc) l'
+  and restart acc =
+    function
+    | x::y::xs when x = y -> (scan [@tailcal]) acc x 2 xs
+    | x::xs -> (restart [@tailcal]) ((One x)::acc) xs
+    | [] -> acc
+  in 
+  restart [] l |> List.rev
+
+(* 12: decode a run-length encoded list *)
+let decode l =
+  let rec aux acc =
+    function
+    | [] -> acc
+    | (One x)::tail -> (aux [@tailcal]) (x::acc) tail
+    | (Many (n, x))::tail -> (aux [@tailcal]) (repeat acc x n) tail
+  and repeat acc x n =
+    if n > 0
+    then (repeat [@tailcal]) (x::acc) x (n - 1)
+    else if n = 0
+    then acc
+    else assert false
+  in
+  aux [] @@ List.rev l
+
+(* 13: run-length encoding of a list *)
+(* UNCLEAR: how is it different from 11? *)
+
+(* 14: duplicate the elements of a list *)
+let duplicate l =
+  let rec aux acc =
+    function
+    | [] -> acc
+    | x::xs -> (aux [@tailcal]) (x::x::acc) xs
+  in
+  aux [] @@ List.rev l
+
+(* 15: replicate the elements of a list a given number of times *)
+let replicate l n_times =
+  let rec repeat acc ?(n=n_times) x =
+    if n > 0
+    then (repeat [@tailcal]) (x::acc) x ~n:(n - 1)
+    else if n = 0
+    then acc
+    else assert false
+  in
+  let rec aux acc =
+    function
+    | [] -> acc
+    | x::xs -> (aux [@tailcal]) (repeat acc x) xs
+  in
+  aux [] @@ List.rev l
+
+(* 16: drop every n-th element from a list *)
+let drop list n =
+  assert (n > 0);
+  let rec aux acc k l =
+    match k + 1, l with
+    | _, [] -> acc
+    | k', x::xs when k' = n -> (aux [@tailcal]) acc 0 xs
+    | k', x::xs -> (aux [@tailcal]) (x::acc) k' xs
+  in
+  aux [] 0 list |> List.rev
+
+(* 17: split a list into two parts; the length of the first part is given *)
+let split list n =
+  assert (n >= 0);
+  let rec aux acc k l =
+    match k, l with
+    | 0, tail
+    | _, ([] as tail) -> List.rev acc, tail
+    | _, x::xs -> (aux [@tailcal]) (x::acc) (k - 1) xs
+  in
+  aux [] n list
+
+(* 18: extract slice from a list *)
+let slice list i k =
+  assert (i >= 0);
+  assert (k >= i);
+  let rec aux acc n l =
+    match n, l with
+    | n, x::xs when n < i -> (aux [@tailcal]) acc (n + 1) xs
+    | n, x::xs when n <= k -> (aux [@tailcal]) (x::acc) (n + 1) xs
+    | _ -> List.rev acc
+  in
+  aux [] 0 list
+
